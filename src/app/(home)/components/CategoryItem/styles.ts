478eacd5816d5ce0fdc8bcc36dfbd8ef085cf0ee
mylenavitoriano <mylenabarbosa08@gmail.com>
2023-11-14 12:00:10 -0300

    feat: add Categories and CategoryItem component

--- src/app/(home)/components/CategoryItem/styles.ts
+++ src/app/(home)/components/CategoryItem/styles.ts
@@ -4,8 +4,8 @@ import { Badge as BadgeMantine } from "@mantine/core";
 import styled from "styled-components";
 
 export const Badge = styled(BadgeMantine)`
-    padding-top: 0.75rem;
-    padding-bottom: 0.75rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
     
     &.mantine-Badge-label,
     &.mantine-Badge-section{
