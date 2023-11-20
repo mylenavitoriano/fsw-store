c249e9593fc07bc355cddb05712e9b6413f3549f
mylenavitoriano <mylenabarbosa08@gmail.com>
2023-11-20 13:59:39 -0300

    feat: add computeProductTotalPrice helperfunction

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -1,10 +1,22 @@
 import { Product } from "@prisma/client";
 
-const computeProductTotalPrice = (product: Product) => {
+interface ProductWithTotalPrice extends Product {
+    totalPrice: number;
+}
+
+const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
     if(product.discountPercentage === 0){
         return {
             ...product,
-            totalPrice: product.basePrice,
+            totalPrice: Number(product.basePrice),
         }
+    }
+
+    const totalPrice = 
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+    return {
+        ... product,
+        totalPrice,
     }
 }
