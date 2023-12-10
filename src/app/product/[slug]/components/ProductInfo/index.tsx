2b88158a49a0f2351641b3c27f3d149e0205e070
mylenavitoriano <mylenabarbosa08@gmail.com>
2023-12-10 13:20:22 -0300

    ProductInfo component

--- src/app/product/[slug]/components/ProductInfo/index.tsx
+++ src/app/product/[slug]/components/ProductInfo/index.tsx
@@ -1,56 +1,135 @@
-import { Text } from "@mantine/core";
+"use client";
+
+import { Button, Text, rgba } from "@mantine/core";
 import { ProductWithTotalPrice } from "../../../../../helpers/product";
-import { BadgeDiscountPercentage, ContainerProductInfo, CurrentPrice, InfoPrice } from "./styles";
+import {
+  BadgeDiscountPercentage,
+  ButtonQuantity,
+  ContainerProductInfo,
+  CurrentPrice,
+  GroupButtonsAmount,
+  GroupDescription,
+  GroupLeft,
+  GroupRight,
+  InfoPrice,
+  ShippingFee,
+} from "./styles";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { LuArrowDown } from "react-icons/lu";
+import { FaTruckFast } from "react-icons/fa6";
+import { useState } from "react";
 
 interface ProductInfoProps {
-    product: Pick<
-        ProductWithTotalPrice,
-        "basePrice"
-        | "description"
-        | "discountPercentage"
-        | "totalPrice"
-        | "name"
-    >
+  product: Pick<
+    ProductWithTotalPrice,
+    "basePrice" | "description" | "discountPercentage" | "totalPrice" | "name"
+  >;
 }
 
-const ProductInfo = ({product: { basePrice, totalPrice, description, discountPercentage, name}}: ProductInfoProps) => {
-    return ( 
-        <ContainerProductInfo>
-            <Text size="lg">{name}</Text>
+const ProductInfo = ({
+  product: { basePrice, totalPrice, description, discountPercentage, name },
+}: ProductInfoProps) => {
+  const [quantity, setQuantity] = useState(1);
 
-            <InfoPrice>
-                <CurrentPrice>
-                    <Text size="xl" fw={700}>
-                        {Number(totalPrice).toLocaleString("pt-br", {
-                            style: "currency",
-                            currency: "BRL",
-                        })}
-                    </Text>
-                    {discountPercentage > 0 && 
-                        <BadgeDiscountPercentage color="rgba(80, 51, 195, 1)">
-                            <Text size="xs" fw={700}>
-                                <LuArrowDown size={14} />
-                                {discountPercentage}%
-                            </Text>
-                        </BadgeDiscountPercentage>
-                    }
-                </CurrentPrice>
-                {discountPercentage > 0 && 
-                    <Text size="sm" c="dimmed">
-                        De: <Text span td="line-through">
-                                {Number(basePrice).toLocaleString("pt-br", {
-                                    style: "currency",
-                                    currency: "BRL",
-                                })}
-                        </Text>
-                    </Text>
-                }
-            </InfoPrice>
+  const handleDecreaseQuantity = () => {
+    setQuantity((prev) => (prev == 1 ? prev : prev - 1));
+  };
 
-            
-        </ContainerProductInfo>
-     );
-}
- 
+  const handleIncreaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  return (
+    <ContainerProductInfo>
+      <Text size="lg">{name}</Text>
+
+      <InfoPrice>
+        <CurrentPrice>
+          <Text size="xl" fw={700}>
+            {Number(totalPrice).toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </Text>
+          {discountPercentage > 0 && (
+            <BadgeDiscountPercentage color="rgba(80, 51, 195, 1)">
+              <Text size="xs" fw={700}>
+                <LuArrowDown size={14} />
+                {discountPercentage}%
+              </Text>
+            </BadgeDiscountPercentage>
+          )}
+        </CurrentPrice>
+        {discountPercentage > 0 && (
+          <Text size="sm" c="dimmed">
+            De:{" "}
+            <Text span td="line-through">
+              {Number(basePrice).toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </Text>
+          </Text>
+        )}
+      </InfoPrice>
+
+      <GroupButtonsAmount>
+        <ButtonQuantity
+          variant="outline"
+          size="xs"
+          onClick={handleDecreaseQuantity}
+        >
+          <IoIosArrowBack size={18} />
+        </ButtonQuantity>
+
+        <Text span={true}>{quantity}</Text>
+
+        <ButtonQuantity
+          variant="outline"
+          size="xs"
+          onClick={handleIncreaseQuantity}
+        >
+          <IoIosArrowForward size={18} />
+        </ButtonQuantity>
+      </GroupButtonsAmount>
+
+      <GroupDescription>
+        <Text size="lg" fw={600}>
+          Descrição
+        </Text>
+        <Text c="dimmed" size="sm">
+          {description}
+        </Text>
+      </GroupDescription>
+
+      <Button tt="uppercase" color="rgba(80, 51, 195, 1)" radius="md">
+        Adicionar ao carrinho
+      </Button>
+
+      <ShippingFee>
+        <GroupLeft>
+            <FaTruckFast size={30}/>
+
+            <div>
+            <Text>
+                Entrega via{" "}
+                <Text span={true} fw={600} fs="italic">
+                FSPacket®
+                </Text>
+            </Text>
+            <Text c="rgba(129,98,255,1)">
+                Envio para{" "}
+                <Text span={true} fw={600}>
+                todo Brasil
+                </Text>
+            </Text>
+            </div>
+        </GroupLeft>
+
+        <Text fw={600}>Frete grátis</Text>
+      </ShippingFee>
+    </ContainerProductInfo>
+  );
+};
+
 export default ProductInfo;
--- src/app/product/[slug]/components/ProductInfo/styles.ts
+++ src/app/product/[slug]/components/ProductInfo/styles.ts
@@ -1,6 +1,6 @@
 "use client";
 import styled from "styled-components";
-import { Badge as BadgeMantine } from "@mantine/core";
+import { Badge as BadgeMantine, Button as ButtonMantine } from "@mantine/core";
 
 export const ContainerProductInfo = styled.div`
   display: flex;
@@ -25,3 +25,45 @@ export const BadgeDiscountPercentage = styled(BadgeMantine)`
     align-items: center;
   }
 `;
+
+export const GroupButtonsAmount = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
+export const ButtonQuantity = styled(ButtonMantine)`
+  color: #fff;
+  border-color: var(--dark-gray);
+  padding: 8px;
+`;
+
+export const GroupDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: .75rem;
+  margin: 1.875rem 0;
+`
+
+export const ShippingFee = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--dark-gray);
+  padding: 0.5rem 1.875rem;
+  border-radius: 0.625rem;
+  margin-top: 1.25rem;
+`
+
+export const GroupLeft = styled.div`
+  display: flex;
+  align-items: center;
+  gap: .75rem;
+`
+
+export const GroupRight = styled.div`
+  display: flex;
+  align-items: center;
+  gap: .75rem;
+`
