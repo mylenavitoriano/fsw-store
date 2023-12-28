1fdd940306ddf95ed3057bff72ebab64f84deea2
mylenavitoriano <mylenabarbosa08@gmail.com>
2023-12-28 16:46:52 -0300

    fix: adjusted url images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
     },
     "images": {
-        "domains": ["fsw-store.s3.sa-east-1.amazonaws.com"],
+        "domains": ["fsw-store.s3.sa-east-1.amazonaws.com", "my-fsw-store.s3.us-east-2.amazonaws.com"],
     },
 }
 
